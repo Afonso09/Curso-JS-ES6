8c7b42120100aae978e4aed2dbdb863af06b645d
Afonso09 <afonsos921@gmail.com>
2020-05-05 11:56:21 -0300

    Classes

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
-class Teste{
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro(){
-
+    add(data) {
+        this.data.push(data);
+        console.log(data);
     }
 }
+
+class todoList extends List {
+  // chamar o constructor da classe pai aqui utilizando o SUPER.
+    constructor(){
+       super();
+// então posso criar outros métodos na classe filho.
+       this.usuario = 'Afonso';
+   }
+   mostraUsuario() {
+   console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new todoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario();
